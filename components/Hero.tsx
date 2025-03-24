bdf2cc22905b337713e0c512a6826dd7ded4f8e9
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-03-24 00:12:46 +0000

    Fix: Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,12 @@
-import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+
+import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      {/**
-       *  UI: Spotlights
-       *  Link: https://ui.aceternity.com/components/spotlight
-       */}
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -23,13 +20,13 @@ const Hero = () => {
       </div>
 
       <div
-        className="h-screen w-full bg-black-100 dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
         {/* Radial gradient for the container to give a faded look */}
         <div
-          className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100
-          [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
 
@@ -39,25 +36,22 @@ const Hero = () => {
             Dynamic Web Magic with Next.js
           </p>
 
-
           <TextGenerateEffect
             words="Transforming Concepts into User Experiences"
-            className="text-center text-[40px] md:text-6xl lg:text-6xl"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Diogo, a Software devoloper based in Portugal.
+            Hi! I&apos;m Diogo, a Software Engineer based in Portugal.
           </p>
-          
+
           <a href="#about">
-            <MagicButton 
+            <MagicButton
               title="Show my work"
               icon={<FaLocationArrow />}
               position="right"
-
             />
           </a>
-
         </div>
       </div>
     </div>
