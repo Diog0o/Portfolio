e5e828143c5bb806e031a526619c66418a865cfd
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-03-26 11:43:10 +0000

    Finish Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Grid from "@/components/Grid";
-import { FaHome } from "react-icons/fa";
 import { navItems } from "@/data";
+import RecentProjects from "@/components/RecentProjects";
+import Experience from "@/components/Experience";
+import { TimelineDemo } from "@/components/ui/TimelineDemo";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col scroll-smooth overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
+        <RecentProjects />
+        <TimelineDemo />
+        <Footer />
       </div>
     </main>
   );
